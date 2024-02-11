04a6b49c82a7fb028b50d83d21ac59434adde651
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2024-02-11 21:32:47 +0700

    feat:add players

--- lib/level/level.dart
+++ lib/level/level.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:adventure/actors/player.dart';
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
@@ -9,6 +10,16 @@ class Level extends World {
   FutureOr<void> onLoad() async {
     level = await TiledComponent.load('Level-01.tmx', Vector2.all(16));
     add(level);
+    final spawnPointLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+    for (final spawnPoint in spawnPointLayer!.objects) {
+      switch (spawnPoint.class_) {
+        case 'Player':
+          final player = Player(
+              character: "Mask Dude",
+              position: Vector2(spawnPoint.x, spawnPoint.y));
+          add(player);
+      }
+    }
     return super.onLoad();
   }
 }
