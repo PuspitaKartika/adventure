45be07b5623f87098575bdb6bdfbb1a34c55e31a
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2024-02-11 08:44:36 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:adventure/pixel_adventure.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+  PixelAdventure game = PixelAdventure();
+  runApp(GameWidget(game: kDebugMode ? PixelAdventure() : game));
+}
---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:adventure/level/level.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xff211F30);
+  late final CameraComponent cam;
+  @override
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
